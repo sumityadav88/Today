7711ecd1e62695f1be451196022b7a7232c47294
Sumit <sumityaadav777@gmail.com>
2024-10-22 16:17:56 +0530

    this is me

---
+++ python.py
@@ -0,0 +1,11 @@
+class sumit:
+    def __init__(self):
+        pass
+
+    def my_function(self):
+        return 'Hi how are you'
+
+object = sumit()
+
+if __name__ == "__main__":
+    print(object.my_function())
